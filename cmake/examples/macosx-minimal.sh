8eebe0f5917afd80b8a7cdbfbc1dd055c688f83c
timoteo dinelli <timoteo.dinelli@polimi.it>
2024-01-25 12:13:37 -0800

    Add options

--- cmake/examples/macosx-minimal.sh
+++ cmake/examples/macosx-minimal.sh
@@ -1,7 +1,9 @@
 cmake \
     -D CMAKE_CXX_COMPILER:PATH=/opt/homebrew/opt/llvm/bin/clang++ \
+    -D CMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
     -D Eigen3_DIR:PATH=/Users/tdinelli/NumericalLibraries/eigen/eigen-3.4.0/share/eigen3/cmake \
     -D Boost_ROOT:PATH=/Users/tdinelli/NumericalLibraries/boost/boost-1.83.0-clang-17.0.1 \
     -D OpenSMOKEpp_ROOT:PATH=/Users/tdinelli/Documents/GitHub/OpenSMOKEpp \
-    -D CMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
+    -D OpenSMOKEppSolvers_ROOT:PATH=/Users/tdinelli/Documents/GitHub/OpenSMOKEppSolvers \
+    -D BLAS_ROOT:PATH=/Users/tdinelli/NumericalLibraries/openblas/openblas-0.3.24-clang-17.0.3 \
     ..
