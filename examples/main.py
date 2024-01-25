4317369c1fcd2f0d70fca19d64ffdf6b585e75a2
timoteo dinelli <timoteo.dinelli@polimi.it>
2024-01-24 19:32:50 -0800

    Update example

--- examples/main.py
+++ examples/main.py
@@ -2,42 +2,17 @@ import os
 import sys
 
 if os.path.isdir("/Users/tdinelli"):
-    sys.path.append(
-        '/Users/tdinelli/Documents/GitHub/pyOpenSMOKEpp/build/Debug/interfaces/python')
+    sys.path.append('/Users/tdinelli/Documents/GitHub/OpenSMOKEpp_Interfaces/build/Debug/interfaces/python')
 
-from pyOpenSMOKE import OpenSMOKEMaps, ThermodynamicsMap_CHEMKIN
-# import numpy as np
-# import matplotlib.pyplot as plt
+from pyOpenSMOKE import OpenSMOKEMaps
 
 maps = OpenSMOKEMaps(
-    "/Users/tdinelli/Documents/GitHub/OpenSMOKEppTutorials/examples/OpenSMOKEpp_BatchReactor/01a-isothermal-constantvolume/kinetics", False)
+    "/Users/tdinelli/Documents/GitHub/OpenSMOKEppTutorials/examples/OpenSMOKEpp_BatchReactor/01a-isothermal-constantvolume/kinetics", True)
 
 maps.ReadMechanism()
 
-
 thermo = maps.ThermodynamicsMap()
-# kinetics = maps.KineticsMap()
-
-
-# thermo = maps.thermodynamicsMap
-# thermo = ThermodynamicsMap_CHEMKIN(maps.ThermodynamicsMap())
+kinetics = maps.KineticsMap()
 
 print("Number Of Species: ", thermo.NumberOfSpecies())
 print("MWs: ", thermo.MWs())
-
-
-# kinetics = maps.kineticsMap
-
-# k = []
-# T = np.linspace(300, 2000, 100)
-#
-# for i in T:
-# 	kinetics.SetTemperature(i)
-# 	kinetics.KineticConstants()
-# 	k.append(kinetics.kArrheniusModified()[1110])
-#
-#
-# x = [1000/i for i in T]
-# plt.plot(x, k, 'r-o')
-# plt.yscale('log')
-# plt.show()
