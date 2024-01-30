efe5410d2d5c7c6ca6065d32045bf304ad8e1212
timoteo dinelli <timoteo.dinelli@polimi.it>
2024-01-29 16:18:34 -0800

    Examples updated

--- examples/batch.py
+++ examples/batch.py
@@ -3,38 +3,44 @@ import sys
 
 if os.path.isdir("/Users/tdinelli"):
     sys.path.append(
-        '/Users/tdinelli/Documents/GitHub/OpenSMOKEpp_Interfaces/build/Debug/interfaces/python')
+        "/Users/tdinelli/Documents/GitHub/OpenSMOKEpp_Interfaces/build/Release/source/python")
 
 from pyOpenSMOKE import OpenSMOKEMaps, BatchReactor
 
 mech = OpenSMOKEMaps(
-    "/Users/tdinelli/Documents/GitHub/OpenSMOKEppTutorials/examples/OpenSMOKEpp_BatchReactor/13a-ignition-delay-times/kinetics", True)
+    "/Users/tdinelli/Documents/GitHub/OpenSMOKEppTutorials/examples/OpenSMOKEpp_BatchReactor/13a-ignition-delay-times/kinetics",
+    False)
 
 mech.ReadMechanism()
 
 thermo = mech.ThermodynamicsMap()
 kinetics = mech.KineticsMap()
 
+names = thermo.NamesOfSpecies()
+# print(names)
+
 reactor = BatchReactor()
+
 reactor.SetType("NonIsothermal-ConstantVolume")
-
-reactor.SetKinetics(kinetics)
-reactor.SetThermodynamic(thermo)
-
 reactor.SetTemperature(700, "K")
 reactor.SetPressure(20, "bar")
 
-reactor.SetInitialComposition("MassFractions",
-                              ["N2", "O2", "NC7H16"],
-                              [7.193901e-01, 2.184260e-01, 6.218394e-02])
+
+A_list = [kinetics.A(0), kinetics.A(0)-10000]
 
 reactor.SetStartTime(0, "s")
-reactor.SetEndTime(1, "s")
+reactor.SetEndTime(0.1, "s")
 reactor.SetOdeOptions()
-reactor.SetBatchOptions(False)
-reactor.Solve()
+reactor.SetBatchOptions(True)
 
-names = thermo.NamesOfSpecies()
-xf = reactor.xf()
-print(reactor.Tf())
-print(reactor.Pf())
+
+for A in A_list:
+    kinetics.Set_A(0, A)
+    reactor.SetKinetics(kinetics)
+    reactor.SetThermodynamic(thermo)
+    reactor.SetInitialComposition("MassFractions",
+                                  ["N2", "O2", "NC7H16"],
+                                  [7.193901e-01, 2.184260e-01, 6.218394e-02])
+    reactor.SetAdditionalOptions()
+    reactor.Solve()
+    print(reactor.xf()[0])
--- examples/main.py
+++ examples/main.py
@@ -2,17 +2,17 @@ import os
 import sys
 
 if os.path.isdir("/Users/tdinelli"):
-    sys.path.append('/Users/tdinelli/Documents/GitHub/OpenSMOKEpp_Interfaces/build/Debug/interfaces/python')
+    sys.path.append('/Users/tdinelli/Documents/GitHub/OpenSMOKEpp_Interfaces/build/Debug/source/python')
 
 from pyOpenSMOKE import OpenSMOKEMaps
 
 maps = OpenSMOKEMaps(
-    "/Users/tdinelli/Documents/GitHub/OpenSMOKEppTutorials/examples/OpenSMOKEpp_BatchReactor/13a-ignition-delay-times/kinetics", True)
+    "/Users/tdinelli/Documents/GitHub/OpenSMOKEppTutorials/examples/OpenSMOKEpp_BatchReactor/13a-ignition-delay-times/kinetics", False)
 
 maps.ReadMechanism()
 
-thermo = maps.ThermodynamicsMap()
-kinetics = maps.KineticsMap()
-
-print("Number Of Species: ", thermo.NumberOfSpecies())
-print("MWs: ", thermo.MWs())
+# thermo = maps.ThermodynamicsMap()
+# kinetics = maps.KineticsMap()
+#
+# print("Number Of Species: ", thermo.NumberOfSpecies())
+# print("MWs: ", thermo.MWs())
