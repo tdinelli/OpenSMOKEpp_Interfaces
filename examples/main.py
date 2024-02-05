7d27adc5dad2245528474c6958c23dd395eeb5bd
timoteo dinelli <timoteo.dinelli@polimi.it>
2024-02-05 14:57:24 -0800

    Update examples

--- examples/main.py
+++ examples/main.py
@@ -1,18 +1,12 @@
-import os
-import sys
-
-if os.path.isdir("/Users/tdinelli"):
-    sys.path.append('/Users/tdinelli/Documents/GitHub/OpenSMOKEpp_Interfaces/build/Debug/source/python')
-
 from pyOpenSMOKE import OpenSMOKEMaps
 
 maps = OpenSMOKEMaps(
-    "/Users/tdinelli/Documents/GitHub/OpenSMOKEppTutorials/examples/OpenSMOKEpp_BatchReactor/13a-ignition-delay-times/kinetics", False)
+    "/Users/tdinelli/Documents/GitHub/OpenSMOKEppTutorials/examples/OpenSMOKEpp_BatchReactor/13a-ignition-delay-times/kinetics", True)
 
 maps.ReadMechanism()
 
-# thermo = maps.ThermodynamicsMap()
-# kinetics = maps.KineticsMap()
-#
-# print("Number Of Species: ", thermo.NumberOfSpecies())
-# print("MWs: ", thermo.MWs())
+thermo = maps.ThermodynamicsMap()
+kinetics = maps.KineticsMap()
+
+print("Number Of Species: ", thermo.NumberOfSpecies())
+print("MWs: ", thermo.MWs())
